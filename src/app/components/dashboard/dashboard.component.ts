d623dd817a12542029dc161179e15d62f405db45
Geoff Mnr <97231176+Geoff-Mnr@users.noreply.github.com>
2024-06-17 17:27:22 +0200

    update

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -19,6 +19,7 @@ import { GlobalStateService } from "../../services/global.service";
   styleUrls: ["./dashboard.component.scss"],
 })
 export class DashboardComponent implements OnInit, OnDestroy {
+  //Injection des services router, darkModeService, authService, userService et toastr
   router = inject(Router);
   darkModeService: DarkModeService = inject(DarkModeService);
   authService = inject(AuthService);
@@ -26,19 +27,24 @@ export class DashboardComponent implements OnInit, OnDestroy {
   toastr = inject(ToastrService);
   private subDelete: Subscription | undefined;
   globalStateService = inject(GlobalStateService);
-
   isAdmin: boolean = false;
 
+  //Constructeur de la classe DashboardComponent avec le service ActivatedRoute en paramètre
   constructor(private route: ActivatedRoute) {}
 
+  //Déclaration de la variable selectedUser de type User
   selectedUser?: User;
-
+  //Déclaration de la variable user
   user: any;
+  //Déclaration de la variable showMenu de type boolean initialisée à false
   showMenu = false;
+
+  //Déclaration de la variable isMenuOpen de type boolean initialisée à false
   isMenuOpen = false;
+
+  //Déclaration de la méthode selectUser qui prend en paramètre un utilisateur de type User
   selectUser(user: User) {
     this.selectedUser = user;
-    console.log("Selected user", this.selectedUser);
     const navigationExtras: NavigationExtras = {
       state: {
         user: user,
@@ -47,22 +53,27 @@ export class DashboardComponent implements OnInit, OnDestroy {
     this.router.navigate(["/user/edit-user"], navigationExtras);
   }
 
+  //Déclaration de la méthode toggleDarkMode qui permet de mettre à jour le mode sombre
   toggleDarkMode() {
     this.darkModeService.updateDarkMode();
   }
 
+  //Déclaration de la méthode toggleMenuDashboard qui permet d'ouvrir ou de fermer le menu
   toggleMenuDashboard() {
     this.isMenuOpen = !this.isMenuOpen;
   }
 
+  //Déclaration de la méthode closeMenuDashboard qui permet de fermer le menu
   closeMenuDashboard() {
     this.isMenuOpen = false;
   }
 
+  //Déclaration de la méthode toggleMenu qui permet d'ouvrir ou de fermer le menu
   toggleMenu() {
     this.showMenu = !this.showMenu;
   }
 
+  //Initialisation de la méthode ngOnInit
   ngOnInit() {
     this.route.params.subscribe(() => {
       this.getProfile();
@@ -70,23 +81,26 @@ export class DashboardComponent implements OnInit, OnDestroy {
     this.isAdmin = this.authService.isAdmin();
   }
 
+  //Déclaration de la méthode getProfile qui permet de récupérer le profil de l'utilisateur
   getProfile() {
-    this.userService.getProfileUser().subscribe((response: any) => {
+    this.subDelete = this.userService.getProfileUser().subscribe((response: any) => {
       this.user = response.data;
-      console.log("User", this.user);
     });
   }
 
+  //Déclaration de la méthode logout qui permet de se déconnecter
   logout() {
     this.authService.logout();
     this.toastr.info("Deconnexion reussie");
     this.router.navigate(["/login"]);
   }
 
+  //Déclaration de la méthode closeMenu qui permet de fermer le menu
   closeMenu() {
     this.showMenu = false;
   }
 
+  //Déclaration de la méthode ngOnDestroy
   ngOnDestroy() {
     if (this.subDelete) {
       this.subDelete.unsubscribe();
